7b42ee27129021c8518b4ec8e559121513fdb3b9
dennismasinde <chengwadennismasinde@gmail.com>
2023-05-15 21:14:34 +0300

    fix producer controller

--- src/main/java/io/maddennis/kafkapoc/controller/KafkaProducerController.java
+++ src/main/java/io/maddennis/kafkapoc/controller/KafkaProducerController.java
@@ -1,7 +1,10 @@
 package io.maddennis.kafkapoc.controller;
 
 import io.maddennis.kafkapoc.service.KafkaProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
