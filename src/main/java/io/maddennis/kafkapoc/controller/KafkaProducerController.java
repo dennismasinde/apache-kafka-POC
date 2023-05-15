b884b0b222a1c0f24460ea6d992467ceffc39b0f
dennismasinde <chengwadennismasinde@gmail.com>
2023-05-15 20:50:01 +0300

    kafka POC

---
+++ src/main/java/io/maddennis/kafkapoc/controller/KafkaProducerController.java
@@ -0,0 +1,22 @@
+package io.maddennis.kafkapoc.controller;
+
+import io.maddennis.kafkapoc.service.KafkaProducer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class KafkaProducerController {
+
+    private final KafkaProducer kafkaProducer;
+
+    public KafkaProducerController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @GetMapping("/publish")
+    public ResponseEntity<String> publish(@RequestParam("message") String message){
+        kafkaProducer.sendMessage(message);
+        return ResponseEntity.ok("Message sent to kafka topic");
+    }
+}
